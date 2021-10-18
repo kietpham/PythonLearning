69fbef783f99af5581bc6a4cc0557f21af12ee99
kiet pham <kietfriendly@gmail.com>
2021-10-18 20:57:46 +0700

    Update views.py

--- Learn01/DjangoAPI/EmployeeApp/views.py
+++ Learn01/DjangoAPI/EmployeeApp/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
 
-from EmployeeApp.models import Departments,Employees
+from EmployeeApp.models import Departments, Employees
 from EmployeeApp.serializers import DepartmentSerializer, EmployeeSerializers
 
 # Create your views here.
